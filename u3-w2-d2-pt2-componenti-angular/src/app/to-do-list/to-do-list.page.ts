b3fdc0b70aa4c94bedce8e3ff0de577b7b36490d
Luigi Indaco <luigindaco@gmail.com>
2022-11-02 22:22:55 +0100

    to do list exercise complete

--- u3-w2-d2-pt2-componenti-angular/src/app/to-do-list/to-do-list.page.ts
+++ u3-w2-d2-pt2-componenti-angular/src/app/to-do-list/to-do-list.page.ts
@@ -2,13 +2,34 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './to-do-list.page.html',
-  styleUrls: ['./to-do-list.page.scss']
+  styleUrls: ['./to-do-list.page.scss'],
 })
 export class ToDoListPage implements OnInit {
+  todos = [
+    {
+      id: 1,
+      text: 'todo a caso',
+      check: '',
+    },
+    {
+      id: 2,
+      text: 'todo a caso due',
+      check: 'check',
+    },
+    {
+      id: 3,
+      text: 'todo a caso tre',
+      check: '',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  removeTodo(i: number) {
+    this.todos.splice(i, 1);
   }
-
+  checkTodo(i: number): void {
+    this.todos[i].check = this.todos[i].check === 'check' ? '' : 'check';
+  }
 }
